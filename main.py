2c006b8618399f669e6204c5b4a26b99c5fa14fb
William Gyrulf <william.gyrulf@hotmail.com>
2025-11-24 20:56:01 +0100

    Fixed env function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv("Credentials/TMDB_key_credentials.env")
+def get_tmdb_credentials():
+    load_dotenv("Credentials/TMDB_key_credentials.env")
+    return os.getenv("TMDB_ACCESS_TOKEN"), os.getenv("TMDB_API_KEY")
 
-TMDB_ACCESS_TOKEN = os.getenv("TMDB_ACCESS_TOKEN")
-TMDB_API_KEY = os.getenv("TMDB_API_KEY")
+TMDB_ACCESS_TOKEN, TMDB_API_KEY = get_tmdb_credentials()
